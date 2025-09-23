fe1c4afe2dfc0d34a259dc74addefd382049e1dc
Bjarke Gotfredsen <gotfredsen@users.noreply.github.com>
2025-09-23 09:54:30 +0200

    Prune libs for springbot (ESP32-S2) board

--- scripts/prune-for-esp32s2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Keep only ESP32-S2 related boards
-BOARDS_DIR="libs"
-KEEP_BOARD="springbot"
-
-# Remove all other boards except springbot
-find $BOARDS_DIR -mindepth 1 -maxdepth 1 -type d ! -name "$KEEP_BOARD" -exec rm -rf {} +
